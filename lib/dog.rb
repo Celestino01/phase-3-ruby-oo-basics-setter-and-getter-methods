f09146b449f5ff3965a1ee715a46011dded660b2
celestino <celestino.martinez01@gmail.com>
2022-07-23 20:54:41 -0400

    commit tree 4787397d91de1c6b2c8a749181c62897255c196b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
 
+    def name=(dog_name)
+        @name = dog_name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
+
+frido = Dog.new
+frido.name=("frido")
+
+snoopy = Dog.new
+snoopy.name=("snoopy")
+snoopy.breed=("Beagle")
